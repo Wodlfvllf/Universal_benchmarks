19cc8204e87c0b1e79591d2fe32d4f0170c01472
Wodlfvllf <shuklashashankshekhar863@gmail.com>
2025-09-30 17:53:45 +0000

    benchmarks

--- tasks/multiple_choice/task.py
+++ tasks/multiple_choice/task.py
@@ -1,6 +1,6 @@
 from typing import List, Dict, Any
 from ..base import BaseTask, TaskInput, TaskOutput
-from ...metrics.registry import MetricRegistry
+from metrics.registry import MetricRegistry
 
 class MultipleChoiceQATask(BaseTask):
     """Implementation for multiple choice question answering"""
